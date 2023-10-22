370aebd9b8c5f025ac563d6bf1379100cfebcbcb
Morathii <morathii@icloud.com>
2023-10-22 12:47:09 +0200

    :sparkles: Initial commit

---
+++ src/main/java/codes/fred/weddingfoodplanner/be/WeddingFoodPlannerBeApplication.java
@@ -0,0 +1,31 @@
+package codes.fred.weddingfoodplanner.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Set;
+
+@SpringBootApplication
+public class WeddingFoodPlannerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingFoodPlannerBeApplication.class, args);
+    }
+
+}
+
+@RestController
+class CustomersHttpController {
+
+    @GetMapping("/customers")
+    Collection<Customer> customers() {
+        return Set.of(new Customer(1, "A"), new Customer(2, "B"), new Customer(3, "C"));
+    }
+
+    record Customer(Integer id, String name) {
+    }
+
+}
---
+++ src/test/java/codes/fred/weddingfoodplanner/be/WeddingFoodPlannerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.fred.weddingfoodplanner.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingFoodPlannerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
